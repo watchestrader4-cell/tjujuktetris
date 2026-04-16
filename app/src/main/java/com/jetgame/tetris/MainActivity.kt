4b29fd2ab57ed6d62a7bf2e1ad04fe1564558e6f
watchestrader4-cell <watchestrader4@gmail.com>
2026-04-16 11:20:44 +0700

    Prepare Tjujuk Tetris for release

---
+++ app/src/main/java/com/jetgame/tetris/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.watchestrader.tetris
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.*
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.watchestrader.tetris.logic.*
+import com.watchestrader.tetris.ui.GameBody
+import com.watchestrader.tetris.ui.GameScreen
+import com.watchestrader.tetris.ui.PreviewGamescreen
+import com.watchestrader.tetris.ui.combinedClickable
+import com.watchestrader.tetris.ui.theme.ComposetetrisTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        SoundUtil.init(this)
+
+        setContent {
+            ComposetetrisTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+
+                    val viewModel = viewModel<GameViewModel>()
+                    val viewState = viewModel.viewState.value
+
+                    LaunchedEffect(key1 = Unit) {
+                        while (isActive) {
+                            delay(650L - 55 * (viewState.level - 1))
+                            viewModel.dispatch(Action.GameTick)
+                        }
+                    }
+
+                    val lifecycleOwner = LocalLifecycleOwner.current
+                    DisposableEffect(key1 = Unit) {
+                        val observer = object : DefaultLifecycleObserver {
+                            override fun onResume(owner: LifecycleOwner) {
+                                viewModel.dispatch(Action.Resume)
+                            }
+
+                            override fun onPause(owner: LifecycleOwner) {
+                                viewModel.dispatch(Action.Pause)
+                            }
+                        }
+                        lifecycleOwner.lifecycle.addObserver(observer)
+                        onDispose {
+                            lifecycleOwner.lifecycle.removeObserver(observer)
+                        }
+                    }
+
+
+                    GameBody(combinedClickable(
+                        onMove = { direction: Direction ->
+                            if (direction == Direction.Up) viewModel.dispatch(Action.Drop)
+                            else viewModel.dispatch(Action.Move(direction))
+                        },
+                        onRotate = {
+                            viewModel.dispatch(Action.Rotate)
+                        },
+                        onRestart = {
+                            viewModel.dispatch(Action.Reset)
+                        },
+                        onPause = {
+                            if (viewModel.viewState.value.isRuning) {
+                                viewModel.dispatch(Action.Pause)
+                            } else {
+                                viewModel.dispatch(Action.Resume)
+                            }
+                        },
+                        onMute = {
+                            viewModel.dispatch(Action.Mute)
+                        }
+                    )) {
+                        GameScreen(
+                            Modifier.fillMaxSize()
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        SoundUtil.release()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposetetrisTheme {
+        GameBody {
+            PreviewGamescreen(Modifier.fillMaxSize())
+        }
+    }
+}
